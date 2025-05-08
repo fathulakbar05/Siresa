e7b53fe176293074e780fd5b2784fa68bbe191d5
artichys <136879284+artichys@users.noreply.github.com>
2025-05-08 21:01:02 +0700

    Delete room-reservation-backend directory

--- room-reservation-backend/models/room.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Room struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Name     string `json:"name"`
-	Location string `json:"location"`
-	Capacity int    `json:"capacity"`
-}
