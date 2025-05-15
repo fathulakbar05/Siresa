346347653641656b005b26c18cbaace0a7bacd38
artichys <136879284+artichys@users.noreply.github.com>
2025-05-15 20:18:42 +0700

    backend-progress

---
+++ go-backend/models/ruangan.go
@@ -0,0 +1,17 @@
+package models
+
+type Ruangan struct {
+    IDRuangan    uint   `gorm:"primaryKey" json:"id_ruangan"`
+    NamaRuangan  string `json:"nama_ruangan"`
+    LokasiGedung string `json:"lokasi_gedung"`
+    Lantai       int    `json:"lantai"`
+    Kapasitas    int    `json:"kapasitas"`
+    Fasilitas    string `json:"fasilitas"`
+    Ukuran       string `json:"ukuran"`
+    Tipe         string `json:"tipe"`
+    Layout       string `json:"layout"`
+}
+
+func (Ruangan) TableName() string {
+    return "ruangan"
+}
