fa6f41cfd32fd07d5df34f04aed0f4ed753c3226
artichys <136879284+artichys@users.noreply.github.com>
2025-05-15 21:57:44 +0700

    Add files via upload

---
+++ go-backend/routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+ 
+ import (
+  "github.com/gin-gonic/gin"
+  "go-backend/controllers"
+ )
+ 
+ func SetupRoutes(router *gin.Engine) {
+  // Rute Reservasi
+  reservasi := router.Group("/reservasi")
+  {
+   reservasi.POST("/ajukan", controllers.AjukanReservasi)
+   reservasi.PUT("/status", controllers.UpdateStatusReservasi)
+   reservasi.GET("/by-status", controllers.GetReservasiByStatus)
+   reservasi.GET("/by-user/:id_user", controllers.GetReservasiByUser)
+  }
+ 
+  // Rute Ruangan
+ruangan := router.Group("/ruangan")
+{
+    ruangan.GET("/", controllers.GetAllRuangan)
+    ruangan.GET("/filter", controllers.FilterRuangan) // Route baru untuk filter
+}
+ 
+  // Rute Auth
+  auth := router.Group("/auth")
+{
+      auth.POST("/login", controllers.Login)
+      auth.POST("/register", controllers.Register)
+}
+}
